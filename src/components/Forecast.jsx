f043e05d0967bba3f5ecd7797a865334d00e1861
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-07-04 21:40:19 -0400

    refined styling

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -3,7 +3,7 @@ import { iconURLFrom } from "../services/openweather";
 
 const Forecast = ({ title, items }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg text-black px-2 mb-auto">
+    <div className="bg-white shadow-lg rounded-lg text-black px-2 mb-auto pb-2">
       <div className="flex items-center justify-start mt-6 ">
         <p className="  font-medium uppercase">{title}</p>
       </div>
@@ -20,38 +20,6 @@ const Forecast = ({ title, items }) => {
           </div>
         ))}
       </div>
-
-      {/* <div className='flex flex-col items-center justify-center'>
-          <p className=' font-light text-sm'>
-            4:00 PM
-          </p>
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" className=' w-12 my-1' alt="" />
-          <p className=' font-medium'> 22°</p>
-        </div>
-       
-        <div className='flex flex-col items-center justify-center'>
-          <p className=' font-light text-sm'>
-            4:00 PM
-          </p>
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" className=' w-12 my-1' alt="" />
-          <p className=' font-medium'> 22°</p>
-        </div>
-       
-        <div className='flex flex-col items-center justify-center'>
-          <p className=' font-light text-sm'>
-            4:00 PM
-          </p>
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" className=' w-12 my-1' alt="" />
-          <p className=' font-medium'> 22°</p>
-        </div>
-       
-        <div className='flex flex-col items-center justify-center'>
-          <p className=' font-light text-sm'>
-            4:00 PM
-          </p>
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" className=' w-12 my-1' alt="" />
-          <p className=' font-medium'> 22°</p>
-        </div> */}
     </div>
   );
 };
